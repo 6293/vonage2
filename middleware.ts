66afeb99d92a26e7f06da298344c1a4da7ef1958
6293 <own7000hr@gmail.com>
2026-04-14 21:53:45 +0900

    log

---
+++ middleware.ts
@@ -0,0 +1,6 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  console.log(`[access] ${request.method} ${request.nextUrl.pathname}`)
+  return NextResponse.next()
+}
